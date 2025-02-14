59b1a5d4ff6e9878a60a2b52c10d3b7e74382c4a
Israelam72 <israelarantes.m@gmail.com>
2025-02-13 22:24:40 -0300

    del: removed contac, and about

--- recipes/views.py
+++ recipes/views.py
@@ -4,9 +4,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse("Sobre")
