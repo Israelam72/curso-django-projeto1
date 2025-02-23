d9295dcf22ebd63b23f6072ba372a029d58a686e
Israelam72 <israelarantes.m@gmail.com>
2025-02-23 18:58:21 -0300

    recipes page beggin

--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Israel',
+    })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Israel',
+    })
