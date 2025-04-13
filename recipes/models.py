fce86b713c1477a93d87ce36dba0910d4fa349d0
Israelam72 <israelarantes.m@gmail.com>
2025-04-13 13:17:36 -0300

    Added category admin model and superuser

--- recipes/models.py
+++ recipes/models.py
@@ -1,8 +1,11 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 
 class Category(models.Model):
   name = models.CharField(max_length=65)
+
+  def __str__(self):
+    return self.name
 
 class Recipe(models.Model):
   title = models.CharField(max_length=65)
@@ -18,7 +21,11 @@ class Recipe(models.Model):
   updated_at = models.DateField(auto_now=True)
   is_published = models.BooleanField(default=False)
   cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d')
-  category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-  author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+  category = models.ForeignKey(
+    Category, on_delete=models.SET_NULL, null=True
+    )
+  author = models.ForeignKey(
+    User, on_delete=models.SET_NULL, null=True
+    )
 
 
